7016ebfe6832799e0826f9da453d61ebb2df5966
dengailin <729880819@qq.com>
2015-11-07 23:58:51 +0800

    steepest ascent hill climbing

---
+++ 4_4_local_search/random_seed.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+# author: ailin
+# output some random numbers to test.txt
+
+import random
+
+def main():
+    f = open("test.txt", "w")
+    for k in xrange(1, 1000):
+        for num in random.sample([i for i in xrange(8)], 8):
+            f.writelines(str(num) + " ")
+        f.writelines("\r\n")
+    f.close()
+
+if __name__ == "__main__":
+    main()
